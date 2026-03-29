061086f43f8d76700640d15e7013cec88a6c17ce
Cristian <cebotaricristian92@gmail.com>
2026-03-29 21:44:49 +0300

    Add metrics endpoint and configure TypeScript support

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import metricsRoutes from "./routes/metrics.routes.js";
 
 const app = express();
 const port = 3001
 
 app.use(cors());
-
-app.get('/api/metrics', (_req, res) => {
-  res.json({status: "ok"});
-});
+app.use('/api', metricsRoutes)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
