30c79ae3ccb0edaa59daa5984ffa27db4f9bda97
Cristian <cristian.cebotari@endava.com>
2026-04-06 10:42:03 +0300

    Fix responsiveness issue for the cpu metric on small screens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import SystemHeader from "./features/system-header/SystemHeader.tsx";
 const App = () => {
   return (
     <main className="xl:flex space-y-3 gap-3 min-h-screen bg-zinc-950/95 px-3 py-3 sm:px-6 sm:py-6">
-      <div className="min-w-110">
+      <div className="sm:w-110">
         <SystemHeader/>
       </div>
 
